aee04cfffd631bdaf947f2b27ca4085de2b94440
Devohka <110058157+Devohka@users.noreply.github.com>
2024-07-13 12:11:34 +0300

    css/style

--- src/componets/Title/Title.jsx
+++ src/componets/Title/Title.jsx
@@ -1,4 +1,8 @@
+import style from"./Title.module.css";
+console.log(style)
 const Title = () => {
-  return <h1>Recipes</h1>;
-};
+  return <h1 style={{color: "red", textShadow: "5px 3px 7px black"}} className={style.decor}>Recipes</h1>;
+  // це інлайнові стилі і використовувати їх ми не будемо!
+}
+
 export default Title;
